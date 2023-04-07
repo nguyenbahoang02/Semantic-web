414e6a7b6741e5cdc5d80d4460ebfc15bad9c330
nguyenbahoang02 <nguyenbahoang02@gmail.com>
2023-04-07 23:24:06 +0700

    crawl and convert ad to semantic web format

---
+++ semanticWeb/src/classes/AdministrativeDivision.java
@@ -0,0 +1,61 @@
+package classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class AdministrativeDivision {
+	String name;
+	String boarderDivision;
+	List<String> narrowerDivision = new ArrayList<>();
+	
+	public AdministrativeDivision(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBoarderDivision() {
+		return boarderDivision;
+	}
+
+	public void setBoarderDivision(String boarderDivision) {
+		this.boarderDivision = boarderDivision;
+	}
+
+	
+	public List<String> getNarrowerDivision() {
+		return narrowerDivision;
+	}
+
+	public void setNarrowerDivision(List<String> narrowerDivision) {
+		this.narrowerDivision = narrowerDivision;
+	}
+
+	public void addNarrowerDivision(String narrowerDivision) {
+		this.narrowerDivision.add(narrowerDivision);
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		AdministrativeDivision oAD = (AdministrativeDivision) o;
+		if(this.name.equals(oAD.name)) {
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		return gson.toJson(this);
+	}
+}
--- semanticWeb/src/semanticWeb/Main.java
+++ semanticWeb/src/semanticWeb/Main.java
@@ -1,77 +1,37 @@
 package semanticWeb;
 
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
-import org.apache.jena.datatypes.RDFDatatype;
 import org.apache.jena.datatypes.xsd.XSDDatatype;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
-import org.apache.jena.ontology.OntProperty;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
-import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.rdf.model.Statement;
-import org.apache.jena.rdf.model.StmtIterator;
-import org.apache.jena.rdfconnection.RDFConnection;
-import org.apache.jena.rdfconnection.RDFConnectionFactory;
-import org.apache.jena.riot.Lang;
-import org.apache.jena.riot.RDFDataMgr;
-import org.apache.jena.util.iterator.ExtendedIterator;
-import org.apache.jena.vocabulary.XSD;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
+import classes.AdministrativeDivision;
 import classes.HistoricalFigure;
+import crawler.Config;
 import crawler.HistoricalFigureCrawler;
-import jakarta.json.JsonException;
 
 public class Main {
 	
 	private static OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
 
-	public static void main(String[] args) throws IOException, ParseException {
-		
+	
+	public static void addDataToOntology() throws IOException, ParseException {
 		model.read("Tourism_Ontology.owl");
-//		model.write(System.out);
-
-		
-//		HistoricalFigureCrawler historicalFigureCrawler = new HistoricalFigureCrawler();
-//		List<HistoricalFigure> listHistoricalFigures = new ArrayList<>();
-//		
-//		JSONParser jsonParser = new JSONParser();
-//        FileReader reader = new FileReader("file\\historicalFigures.json");
-//        JSONArray objectArray = (JSONArray) jsonParser.parse(reader);
-//		List<String> nameList = new ArrayList<String>();
-//        
-//		for(int i=0; i<objectArray.size(); i++) {
-//			JSONObject object = (JSONObject) objectArray.get(i);
-//			nameList.add(object.get("name").toString());
-//		}
-//		
-//		reader = new FileReader("file\\query.json");
-//        objectArray = (JSONArray) jsonParser.parse(reader);
-//		
-//        listHistoricalFigures.addAll(historicalFigureCrawler.historicalFiguresFilter(objectArray, nameList));
-//        historicalFigureCrawler.writeDatatoFileJSON(listHistoricalFigures);
-        
-		
 		String base = "https://www.culturaltourism.vn/ontologies#";
 		
 		
@@ -93,10 +53,55 @@ public class Main {
 		model.setNsPrefix("ontologies1", "https://www.culturaltourism.vn/ontologies/");
 		
 		
+		List<AdministrativeDivision> administrativeDivisions = new ArrayList<>();
+		administrativeDivisions.addAll(readADFile());
 		
 		JSONParser jsonParser = new JSONParser();
-        FileReader reader = new FileReader("file\\betterHistoricalFigures.json");
+        FileReader reader = new FileReader("file\\testAd.json");
         JSONArray objectArray = (JSONArray) jsonParser.parse(reader);
+		
+        for(int i =0;i<objectArray.size();i++){
+        	JSONObject object = (JSONObject) objectArray.get(i);
+        	
+        	Resource subject = model.createResource(base + object.get("name").toString().replaceAll(" ", "_"));
+        	Property predicate = model.createProperty("http://www.w3.org/1999/02/22-rdf-syntax-ns#type");
+        	Resource classType = model.createClass("https://www.culturaltourism.vn/ontologies#AdministrativeDivision");
+        	
+        	model.add(subject, predicate, classType);
+        	
+//        	if(object.get("boarderDivision") != null) {
+//        		String fatherAd = object.get("boarderDivision").toString().replaceAll(" ", "_");
+//        		
+//        		Resource boarderStatement = model.createResource();
+//        		boarderStatement.addProperty(model.createProperty("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), model.createClass(base + "Statement"));
+//        		
+//        		Resource ad = model.createResource(base + fatherAd);
+//        		boarderStatement.addProperty(model.createObjectProperty(base + "_boarderDivision"), ad);
+//        		
+//        		model.add(subject, model.createAnnotationProperty(base + "boarderDivision"), boarderStatement);
+//        		
+//        	}
+//        	
+//        	JSONArray array = (JSONArray) object.get("narrowerDivision");
+//        	if(array != null) {
+//        		for (Object narrowerDivision : array) {
+//        			String sonAd = narrowerDivision.toString().replaceAll(" ", "_");
+//            		
+//            		Resource narrowerStatement = model.createResource();
+//            		narrowerStatement.addProperty(model.createProperty("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), model.createClass(base + "Statement"));
+//            		
+//            		Resource ad = model.createResource(base + sonAd);
+//            		narrowerStatement.addProperty(model.createObjectProperty(base + "_narrowerDivision"), ad);
+//            		
+//            		model.add(subject, model.createAnnotationProperty(base + "narrowerDivision"), narrowerStatement);
+//				}
+//        	}
+        }
+        
+        
+		jsonParser = new JSONParser();
+        reader = new FileReader("file\\betterHistoricalFigures.json");
+        objectArray = (JSONArray) jsonParser.parse(reader);
         
         for(int i =0;i<objectArray.size();i++){
         	JSONObject object = (JSONObject) objectArray.get(i);
@@ -156,13 +161,16 @@ public class Main {
         	}
         	
         	try {
-        		
+        		String birthPlace = object.get("birthPlace").toString();
+        		if(checkAd(administrativeDivisions, birthPlace)!=null) {
+        			birthPlace = checkAd(administrativeDivisions, birthPlace);
+        		}
         		Resource bornInDescription = model.createResource();
         		bornInDescription.addProperty(model.createProperty("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), model.createClass(base + "Statement"));
         		
-        		Resource placeResource = model.createResource(base + object.get("birthPlace").toString().replaceAll(" ", "_"));
+        		Resource placeResource = model.createResource(base + birthPlace.replaceAll(" ", "_"));
         		placeResource.addProperty(model.createProperty("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), model.createClass(base + "AdministrativeDivision"));
-
+        		
         		bornInDescription.addProperty(model.createObjectProperty(base + "_birthPlace"), placeResource);
         		
         		Resource resource = model.createResource();
@@ -178,11 +186,14 @@ public class Main {
         	}
         	
         	try {
-        		
+        		String deathPlace = object.get("deathPlace").toString();
+        		if(checkAd(administrativeDivisions, deathPlace)!=null) {
+        			deathPlace = checkAd(administrativeDivisions, deathPlace);
+        		}
         		Resource bornInDescription = model.createResource();
         		bornInDescription.addProperty(model.createProperty("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), model.createClass(base + "Statement"));
         		
-        		Resource placeResource = model.createResource(base + object.get("deathPlace").toString().replaceAll(" ", "_"));
+        		Resource placeResource = model.createResource(base + deathPlace.replaceAll(" ", "_"));
         		placeResource.addProperty(model.createProperty("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), model.createClass(base + "AdministrativeDivision"));
         		
         		bornInDescription.addProperty(model.createObjectProperty(base + "_deathPlace"), placeResource);
@@ -203,11 +214,130 @@ public class Main {
         
         OutputStream out = new FileOutputStream("output.rdf");
         model.write(out, "RDF/XML");
-		
-		
+	}
 	
-
+	public static String checkAd(List<AdministrativeDivision> list, String name) {
+		for (AdministrativeDivision administrativeDivision : list) {
+			if(administrativeDivision.getName().contains(name)) return administrativeDivision.getName();
+		}
+		return null;
+	}
+	
+	public static List<AdministrativeDivision> readADFile() throws IOException, ParseException {
+		List<AdministrativeDivision> listAdministrativeDivisions = new ArrayList<>();
+		
+		JSONParser jsonParser = new JSONParser();
+        FileReader reader = new FileReader("file\\AD_lv1.json");
+        JSONArray adLv1 = (JSONArray) jsonParser.parse(reader);
+		
         
+		for(int i = 0; i<adLv1.size(); i++) {
+			JSONObject object = (JSONObject) adLv1.get(i);
+			listAdministrativeDivisions.add(new AdministrativeDivision(object.get("Tên").toString()));
+		}
+	
+		reader = new FileReader("file\\AD_lv2.json");
+		JSONArray adLv2 = (JSONArray) jsonParser.parse(reader);
+        
+		
+		for(int i = 0; i<adLv2.size(); i++) {
+			JSONObject object = (JSONObject) adLv2.get(i);
+			String nameString = object.get("Tên").toString();
+			String fatherString = object.get("Tỉnh, Thành Phố").toString();
+			AdministrativeDivision ad = new AdministrativeDivision(nameString);
+			AdministrativeDivision adFather = new AdministrativeDivision(fatherString);
+			
+			ad.setBoarderDivision(fatherString);
+			listAdministrativeDivisions.get(listAdministrativeDivisions.indexOf(adFather)).addNarrowerDivision(nameString);
+			
+			listAdministrativeDivisions.add(ad);
+			
+			
+		}
+		
+		reader = new FileReader("file\\AD_lv3.json");
+		JSONArray adLv3 = (JSONArray) jsonParser.parse(reader);
+        
+		
+		for(int i = 0; i<adLv3.size(); i++) {
+			JSONObject object = (JSONObject) adLv3.get(i);
+			if(object.get("Phường Xã") == null) continue;
+			String fatherString = object.get("Quận Huyện").toString();
+			String nameString = object.get("Phường Xã").toString();
+			
+			AdministrativeDivision ad = new AdministrativeDivision(nameString);
+			AdministrativeDivision adFather = new AdministrativeDivision(fatherString);
+			
+			ad.setBoarderDivision(fatherString);
+			listAdministrativeDivisions.get(listAdministrativeDivisions.indexOf(adFather)).addNarrowerDivision(nameString);
+			
+			listAdministrativeDivisions.add(ad);
+			
+			
+		}
+		
+//		try {
+//			FileWriter fw = new FileWriter(Config.PATH_FILE + "testAd.json");
+//			fw.write("[\n");
+//			for (int i = 0; i < listAdministrativeDivisions.size(); i++) {
+//				fw.write(listAdministrativeDivisions.get(i).toString());
+//				if (i != listAdministrativeDivisions.size() - 1)
+//					fw.write(",\n");
+//				else
+//					fw.write("\n");
+//			}
+//			fw.write("\n]");
+//			fw.close();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+		
+		return listAdministrativeDivisions;
+	}
+	
+	public static void main(String[] args) throws IOException, ParseException {
+		
+		
+//		model.write(System.out);
+
+		
+//		HistoricalFigureCrawler historicalFigureCrawler = new HistoricalFigureCrawler();
+//		List<HistoricalFigure> listHistoricalFigures = new ArrayList<>();
+//		
+//		JSONParser jsonParser = new JSONParser();
+//        FileReader reader = new FileReader("file\\historicalFigures.json");
+//        JSONArray objectArray = (JSONArray) jsonParser.parse(reader);
+//		List<String> nameList = new ArrayList<String>();
+//        
+//		for(int i=0; i<objectArray.size(); i++) {
+//			JSONObject object = (JSONObject) objectArray.get(i);
+//			nameList.add(object.get("name").toString());
+//		}
+//		
+//		reader = new FileReader("file\\query.json");
+//        objectArray = (JSONArray) jsonParser.parse(reader);
+//		
+//        listHistoricalFigures.addAll(historicalFigureCrawler.historicalFiguresFilter(objectArray, nameList));
+//        historicalFigureCrawler.writeDatatoFileJSON(listHistoricalFigures);
+        
+		
+		addDataToOntology();
+
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
