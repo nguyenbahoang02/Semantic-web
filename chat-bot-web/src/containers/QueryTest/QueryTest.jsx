dde62c603a710d6923704ebc89f8f8730d6724c4
nguyenbahoang02 <nguyenbahoang02@gmail.com>
2023-10-12 16:44:34 +0700

    update

--- chat-bot-web/src/containers/QueryTest/QueryTest.jsx
+++ chat-bot-web/src/containers/QueryTest/QueryTest.jsx
@@ -43,6 +43,11 @@ function QueryTest() {
         setResult(tmp);
       });
   };
+  const chooseQuestion = (current) => {
+    setResultQuery(current.result);
+    setQuery(current.query);
+    setCurrentQuestion(current.question);
+  };
   return (
     <div className="query-test">
       <link
@@ -56,73 +61,120 @@ function QueryTest() {
           {language ? "Tiếng Việt" : "English"}
         </div>
         <div className="content-body">
-          <div className="query">
-            QUERY
-            <div className="big box query-box">
-              {"PREFIX xsd: <http://www.w3.org/2001/XMLSchema#"}
-              <br />
-              {"PREFIX owl: <http://www.w3.org/2002/07/owl#>"}
-              <br />
-              {"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"}
-              <br />
-              {"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"}
-              <br />
-              {"PREFIX prov: <http://www.w3.org/ns/prov#>"}
-              <br />
-              {"PREFIX ontologies: <https://tovie.vn/ontologies#>"}
-              <br />
-              {query}
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              Current question:
-              <br />
-              <br />
-              {currentQuestion}
+          <div className="query-result">
+            <div className="query">
+              QUERY
+              <div className="big box query-box">
+                {"PREFIX xsd: <http://www.w3.org/2001/XMLSchema#"}
+                <br />
+                {"PREFIX owl: <http://www.w3.org/2002/07/owl#>"}
+                <br />
+                {"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"}
+                <br />
+                {"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"}
+                <br />
+                {"PREFIX prov: <http://www.w3.org/ns/prov#>"}
+                <br />
+                {"PREFIX ontologies: <https://tovie.vn/ontologies#>"}
+                <br />
+                {query}
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                Current question:
+                <br />
+                <br />
+                {currentQuestion}
+              </div>
+              {query !== "" && (
+                <div className="run-btn" onClick={execQuery}>
+                  RUN QUERY
+                </div>
+              )}
             </div>
-            {query !== "" && (
-              <div className="run-btn" onClick={execQuery}>
-                RUN QUERY
-              </div>
-            )}
-          </div>
-          <div className="result">
-            RESULT
-            {result !== undefined && (
-              <div className="big box">
-                {result.map((current) => {
-                  return (
-                    <div className="column">
-                      <div className="column-label cell">{current.label}</div>
-                      {current.value.map((currentLabel) => {
-                        return <div className="cell">{currentLabel}</div>;
-                      })}
-                    </div>
-                  );
-                })}
-              </div>
-            )}
+            <div className="result">
+              RESULT
+              {result !== undefined && (
+                <div className="big box">
+                  {result.map((current) => {
+                    return (
+                      <div className="column">
+                        <div className="column-label cell">{current.label}</div>
+                        {current.value.map((currentLabel) => {
+                          return <div className="cell">{currentLabel}</div>;
+                        })}
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
+            </div>
           </div>
           <div className="question-list">
             QUESTIONS LIST
-            <div className="big box">
-              {questions[language ? "vn" : "en"].map((current, index) => {
-                return (
-                  <div
-                    className="small box"
-                    key={index}
-                    onClick={() => {
-                      setResultQuery(current.result);
-                      setQuery(current.query);
-                      setCurrentQuestion(current.question);
-                    }}
-                  >
-                    {current.question}
-                  </div>
-                );
-              })}
+            <div className="question-grid">
+              <div className="column">
+                <div className="column-label cell">
+                  {language ? "Câu hỏi liên quan đến nhân vật lịch sử" : "Questions related to historical figure"}
+                </div>
+                {questions[language ? "vn" : "en"].map((current, index) => {
+                  if (index <= 25)
+                    return (
+                      <div className="cell-index">
+                        <div className="cell index">{index+1}</div>
+                        <div
+                          className="cell"
+                          key={index}
+                          onClick={() => chooseQuestion(current)}
+                        >
+                          {current.question}
+                        </div>
+                      </div>
+                    );
+                })}
+              </div>
+              <div className="column">
+                <div className="column-label cell">
+                  {language ? "Câu hỏi liên quan đến lễ hội" : "Questions related to festival"}
+                </div>
+                {questions[language ? "vn" : "en"].map((current, index) => {
+                  if (index > 25 && index <=29)
+                    return (
+                      <div className="cell-index">
+                        <div className="cell index">{index-25}</div>
+                        <div
+                          className="cell"
+                          key={index}
+                          onClick={() => chooseQuestion(current)}
+                        >
+                          {current.question}
+                        </div>
+                      </div>
+                    );
+                })}
+              </div>
+              <div className="column">
+                <div className="column-label cell">
+                  {language ? "Câu hỏi liên quan đến di tích lịch sử" : "Questions related to historical site"}
+                </div>
+                {questions[language ? "vn" : "en"].map((current, index) => {
+                  if (index > 29)
+                    return (
+                      <div className="cell-index">
+                        <div className="cell index">{index-29}</div>
+                        <div
+                          className="cell"
+                          key={index}
+                          onClick={() => chooseQuestion(current)}
+                        >
+                          {current.question}
+                        </div>
+                      </div>
+                    );
+                })}
+              </div>
             </div>
           </div>
         </div>
