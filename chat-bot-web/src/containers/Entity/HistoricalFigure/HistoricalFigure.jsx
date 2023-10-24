afefd7facb2d974360a7cd213fb47de33df7225f
nguyenbahoang02 <nguyenbahoang02@gmail.com>
2023-10-24 13:08:57 +0700

    update

--- chat-bot-web/src/containers/Entity/HistoricalFigure/HistoricalFigure.jsx
+++ chat-bot-web/src/containers/Entity/HistoricalFigure/HistoricalFigure.jsx
@@ -7,7 +7,8 @@ const HistoricalFigure = () => {
   const [isLoading, setIsLoading] = useState(true);
   const param = useParams();
   const name = capitalizeWords(param.name);
-
+  const [isExisted, setIsExisted] = useState(false);
+  const [entity, setEntity] = useState({});
   const prefix = `PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
   PREFIX owl: <http://www.w3.org/2002/07/owl#>
   PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -25,15 +26,21 @@ const HistoricalFigure = () => {
       body: new URLSearchParams({
         query:
           prefix +
-          `SELECT * WHERE { 
-          ?x a ontologies:HistoricalFigure. 
-          ?x rdfs:label "${name}"@en. 
-          }`,
+        //   `SELECT * WHERE { 
+        //   ?x a ontologies:HistoricalFigure. 
+        //   ?x rdfs:label "${name}"@en. 
+        //   ?x a ?b.
+        //   }`,
+        `DESCRIBE <https://tovie.vn/ontologies#Mạc_Kính_Vũ>
+        `,
       }),
     })
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
+        if (data.results.bindings.length !== 0) {
+          setIsExisted(true);
+        }
         setIsLoading(false);
       })
       .catch((err) => {
@@ -44,8 +51,13 @@ const HistoricalFigure = () => {
 
   return (
     <HistoricalFigurePage>
-      {isLoading && <div>Loading</div>}
-      {!isLoading && <div>Loaded</div>}
+      {isLoading && <div className="loader" />}
+      {!isLoading && (
+        <div>
+          {!isExisted && <div className="not-found">Page not found</div>}
+          {isExisted && <div>Loaded</div>}
+        </div>
+      )}
     </HistoricalFigurePage>
   );
 };
--- chat-bot-web/src/containers/Entity/HistoricalFigure/index.style.js
+++ chat-bot-web/src/containers/Entity/HistoricalFigure/index.style.js
@@ -6,7 +6,34 @@ const HistoricalFigurePage = styled.div`
   --btnC: #d99578;
   --btnHoverColor: #302e2f;
   --borderColor: #e4e4e4;
+  .loader {
+    border: 4px solid rgba(255, 255, 255, 0.3);
+    border-top: 4px solid #3498db;
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    animation: spin 2s linear infinite;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
   @media only screen and (min-width: 1200px) {
+    .not-found {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      font-size: 2rem;
+    }
   }
 `;
 
